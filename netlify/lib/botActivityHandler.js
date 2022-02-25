e0939392843d7808aa2b0adc6d8d6dbb3819f40a
Abrudean Sergiu <sergiu.abrudean@avora.com>
2022-02-25 06:47:27 +0200

    Add basic bot command responses

--- netlify/lib/botActivityHandler.js
+++ netlify/lib/botActivityHandler.js
@@ -6,85 +6,92 @@ class BotActivityHandler extends TeamsActivityHandler {
   constructor() {
     super();
 
+
+        this.onMembersAdded(async (context, next) => {
+
+          const message = "Hello! And welcome to Gravity Teams Integration!   \nThis bot can return information needed to set up a Microsoft Teams Notification Integration in our platform.   \nThe notifications will be based on important events, for example job failiures.";
+
+          for (const idx in context.activity.membersAdded) {
+
+              if (context.activity.membersAdded[idx].id === context.activity.recipient.id) {
+                await context.sendActivity(message);
+              }
+          }
+
+          await next();
+      });
+
+
+  //   this.onMembersAdded(async (context, next) => {
+  //     const membersAdded = context.activity.membersAdded;
+  //     // const membersAdded = context.activity.channelData.teamsAddMembers;
+  //     const message = "Hello! And welcome to Gravity Teams Integration!   \nThis bot can respond to these commands: help, test, user, channel, info; which returns information needed to set up a Microsoft Teams Notification Integration in our platform.   \nThe notifications will be based on important events, for example job failiures.";
+
+  //     let conversationType = context.activity.conversation.conversationType;
+  //     for (let cnt = 0; cnt < membersAdded.length; cnt++) {
+  //         if (membersAdded[cnt].id == context.activity.recipient.id) {
+  //             if (conversationType === 'personal') {
+  //                 //member is added to personal channel
+  //                 await context.sendActivity(message);
+  //             }
+  //         } else if (conversationType !== 'personal') {
+  //           this.startReplyChain(context.activity.serviceUrl, context.activity.channelData.channel.id, message, function (err, address) {
+  //             if (err) {
+  //                 console.log(err);
+  //                 session.endDialog('There is some error');
+  //             }
+  //             // else {
+  //             //     var msg = new teams.TeamsMessage(session)
+  //             //     .text('Reply to main conversation')
+  //             //     .address(address);
+  //             //     session.send(msg);
+  //             //     session.endDialog();
+  //             // }
+  //             }
+  //             );
+  //             //bot is added to channel, send welcome to channel
+  //             await context.sendActivity(message);
+  //         }
+  //     }
+  //     await next();
+  // });
+
     // Registers an activity event handler for the message event, emitted for every incoming message activity.
     this.onMessage(async (context, next) => {
       TurnContext.removeRecipientMention(context.activity);
       const text = context.activity.text.trim().toLocaleLowerCase();
-      if (text.includes("channel")) {
+      if (text.toLowerCase().includes("channel")) {
         if (!context.activity.channelData.channel) {
           await context.sendActivity(
-            `channel must be called from a channel.`
+            `"channel" must be called inside a channel.`
           );
          return;
         }
         await context.sendActivity("Channel ID: "+context.activity.channelData.channel.id);
-//         await this.updateCourierProfile(context);
-      } else if (text.includes("test")) {
+      } else if (text.toLowerCase().includes("test")) {
         await context.sendActivity(`Gravity bot has been successfully added.`);
-      } else if (text.includes("user")) {
+      } else if (text.toLowerCase().includes("hi")) {
+        await context.sendActivity(`Hello! Hope you're having a great day!`);
+      } else if (text.toLowerCase().includes("hello")) {
+        await context.sendActivity(`Hello! Hope you're having a great day!`);
+      } else if (text.toLowerCase().includes("user")) {
         await context.sendActivity("User ID: "+context.activity.from.id);
-      } else if (text.includes("help")) {
-        await context.sendActivity("If you call these commands from a channel use `@Gravity info` format.   \nBot must be added to a channel before calling.  \nAvailable commands: test, info, user, channel");
-      } else if (text.includes("info")) {
+      } else if (text.toLowerCase().includes("help")) {
+        await context.sendActivity("If you call these commands from a channel use `@Gravity info` format.   \nBot must be added to a channel before calling.   \nAvailable commands:   \ntest - checks if Gravity is available,   \ninfo - returns Service URL and Tenant ID used to create integrations,   \nuser - returns User ID used to create policies that send notifications to personal chat,   \nchannel - returns Channel ID used to create policies that send notifications to team channels");
+      } else if (text.toLowerCase().includes("info")) {
         const {
-      serviceUrl: service_url,
-      channelData: {
-        tenant: { id: tenant_id }
-      }
+          serviceUrl: service_url,
+          channelData: {
+            tenant: { id: tenant_id }
+          }
     } = context.activity;
         await context.sendActivity("Service URL: "+service_url+"  \nTenant ID: "+tenant_id);//+"\nuser id: "+context.activity.from.id
       } else {
-        await context.sendActivity("Unknown command!  Available commands: test, info, user, channel");
+        await context.sendActivity("Unknown command!  Available commands: test, info, user, channel, help");
       }
 
       await next();
     });
-  }
-
-  async updateCourierProfile(context) {
-    const {
-      serviceUrl: service_url,
-      channelData: {
-        tenant: { id: tenant_id }
-      }
-    } = context.activity;
-    let profile = {
-      ms_teams: {
-        tenant_id,
-        service_url
-      }
-    };
-    const text = context.activity.text.trim();
-    const [cmd, recipientId] = text.split(" ");
-
-    if (cmd.toLowerCase() === "show-channel") {
-      if (!context.activity.channelData.channel) {
-        await context.sendActivity(
-          `show-channel must be called from a channel.`
-        );
-        return;
-      }
-      await context.sendActivity("channel id: "+context.activity.channelData.channel.id);
-    } else if (cmd.toLowerCase() === "show-user") {
-      await context.sendActivity("user id: "+context.activity.from.id);
-    } else {
-      await context.sendActivity(
-        `Error. Unsupported action.`
-      );
-      return;
-    }
-
-//     try {
-//       await courier.mergeProfile({
-//         recipientId,
-//         profile
-//       });
-
-//       await context.sendActivity(`Your profile has been updated.`);
-//     } catch (err) {
-//       console.log(err);
-//       await context.sendActivity(`An error occurred updating your profile.`);
-//     }
   }
 }
 
